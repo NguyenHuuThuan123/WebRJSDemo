cbd034460f6d1675601a9c7c390146d0278a1b85
user.email <huuthuan170003@gmail.com>
2023-03-19 10:03:26 +0700

    dang nhap

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,52 @@
-import React, {useState} from 'react'
-
+import React, {useState,useEffect} from 'react'
+import { loginUser } from "../Redux/user/userSlice";
 import {Container,Row, Col,Form, FormGroup} from "reactstrap"
 import CommonSection from '../components/UI/CommonSection'
 import Helmet from '../components/Helmet/Helmet'
 import "../styles/checkout.css";
-import { useSelector } from "react-redux";
-import {Link } from "react-router-dom";
+import { useSelector, useDispatch  } from "react-redux";
+import {Link, useNavigate } from "react-router-dom";
 import "../styles/login.css";
+import * as yup from "yup";
+import { Formik, useFormik } from "formik";
+import {toast} from 'react-toastify';
+import CustomInput from "../components/CustomInput";
+
+let schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Email should be valid")
+    .required("Email is Required"),
+  password: yup.string().required("Password is Required"),
+});
+
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const [email,setEmail] = useState('')
-  const [password,setPassword] = useState('')
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      // console.log(values);
+      dispatch(loginUser(values));
+    },
+  });
+
+  const authState = useSelector((state) => state);
+
+  const { user, isSuccess } = authState.auth;
+
+  useEffect(() => {
+    if (user && isSuccess) {
+      // console.log(user);
+      window.location.assign("/");
+    }
+  }, [user, navigate, isSuccess]);
 
   return (
     <Helmet title="Login">
@@ -22,16 +57,25 @@ const Login = () => {
               <h3 className="fw-bold fs-4">
                   Login
               </h3>
-                <Form className="auth__form">
-                  <FormGroup className="form__group">
-                  <input type="email" placeholder="your email" 
-                  value={email} onChange={e => setEmail(e.target.value)}/>
-                  </FormGroup>
+                <Form  action="" className="auth__form" onSubmit={formik.handleSubmit}>
+                <CustomInput type="text"
+                  label="Enter Your Email Address"
+                  id="email"
+                  name="email"
+                  onChng={formik.handleChange("email")}
+                  onBlr={formik.handleBlur("email")}
+                  val={formik.values.email}
+                />
 
-                  <FormGroup className="form__group">
-                  <input type="password" placeholder="your password"
-                   value={password} onChange={e => setPassword(e.target.value)} />
-                  </FormGroup>
+                    <CustomInput
+                  type="password"
+                  label="Password"
+                  id="password"
+                  name="password"
+                  onChng={formik.handleChange("password")}
+                  onBlr={formik.handleBlur("password")}
+                  val={formik.values.password}
+                />
 
                   <button type="submit" className="but__btn auth__btn">Login</button>
                     <p>Create account <Link to='/signup'>hear</Link></p>
